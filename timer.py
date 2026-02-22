1904336b4511138b792511e568e132b37b8b216f
Amir-2010 <Amir.hozhabri.rk2025@gmail.com>
2026-02-22 21:03:35 +0330

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,4 +1,5 @@
 
+# let's make one digital clock!
 from tkinter import *
 from time import strftime
 
@@ -15,7 +16,7 @@ img = PhotoImage(file="E:/Images/Clock.png")
 window.iconphoto(True,img)
 
 label_clock = Label(window,
-                    font=("ds-digital",80),
+                    font=("ds-digital",80), # for make the numbers and characters you have to install this font
                     background="black",
                     foreground="Cyan",
                     height=2)
