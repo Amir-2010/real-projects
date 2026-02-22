09fc0f226372c101ff791bb37011d74144f46de6
Amir-2010 <Amir.hozhabri.rk2025@gmail.com>
2026-02-22 21:02:12 +0330

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,25 @@
+
+from tkinter import *
+from time import strftime
+
+def clock_func():
+    clock_var = strftime(" %H : %M : %S %p ")
+    label_clock.config(text=clock_var)
+    label_clock.after(1000,clock_func)
+
+window = Tk()
+window.resizable(False,False)
+window.title("Clock")
+
+img = PhotoImage(file="E:/Images/Clock.png")
+window.iconphoto(True,img)
+
+label_clock = Label(window,
+                    font=("ds-digital",80),
+                    background="black",
+                    foreground="Cyan",
+                    height=2)
+label_clock.pack(fill="x",anchor="center")
+
+clock_func()
+window.mainloop()
